b3b386455e5eb368ac6ea5234cc0ec7468965e28
Olov Johansson <johansson.olov@gmail.com>
2016-02-08 21:12:18 +0100

    Tests now work without postgres

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,5 @@
 ExUnit.start
 
-Mix.Task.run "ecto.create", ~w(-r DocumentationServer.Repo --quiet)
-Mix.Task.run "ecto.migrate", ~w(-r DocumentationServer.Repo --quiet)
-Ecto.Adapters.SQL.begin_test_transaction(DocumentationServer.Repo)
-
+#Mix.Task.run "ecto.create", ~w(-r DocumentationServer.Repo --quiet)
+#Mix.Task.run "ecto.migrate", ~w(-r DocumentationServer.Repo --quiet)
+#Ecto.Adapters.SQL.begin_test_transaction(DocumentationServer.Repo)
